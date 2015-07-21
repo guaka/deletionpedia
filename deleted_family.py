de67bb1275f25917c1d43baf6fa30c8f62b1468b
Kasper Souren <kasper@guaka.org>
2015-07-22 01:18:39 +0200

    #2 family file

---
+++ deleted_family.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8  -*-
+import family, config
+
+class Family(family.Family):
+    def __init__(self):
+        family.Family.__init__(self)
+        self.name = 'deleted'
+        langlist = [ 'en', 'fr', 'nl', 'de', 'es', 'it', 'pt', 'sv' ]
+        self.langs = { x: x for x in langlist }
+
+    def hostname(self, code):
+        return 'deletionpedia.org'
+
+    def scriptpath(self, code):
+        if code == 'en':
+            return '/w'
+        else:
+            return '/' + code + 'w'
+
+        
